9a491101eb464f32cb35f233d0f39a0ef7883f25
Abhishek Sankar <241abhishek@gmail.com>
2025-06-06 23:52:39 +0530

    Visualize modified ur10e with lift joint added

---
+++ ur10e_mod/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'ur10e_mod'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch',
+            ['launch/visualize_ur10e_mod.launch.xml']),
+        ('share/' + package_name + '/urdf',
+            ['urdf/ur10e_mod.urdf.xacro']),
+        ('share/' + package_name + '/config',
+            ['config/view_robot.rviz']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Abhishek Sankar',
+    maintainer_email='241abhishek@gmail.com',
+    description='Modified UR10e robot description package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
